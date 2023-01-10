000874e50d07deda2754494ab1fd10081fc30a61
Sarahsegla <sarahsegla@icloud.com>
2023-01-10 17:14:08 +0000

    found increase/decrease and value change

--- script.js
+++ script.js
@@ -37,16 +37,45 @@ let totalMonths = finances.length;
 //};
 
 var totalProfitLose = 0;
+var increase = 0; 
+var decrease = Infinity;
+
+var previousMonth = 0;
+
+var greatestMonth;
+var leastMonth;
+var totalChangeValues = 0;
 
 for (var i = 0; i < finances.length; i++) {
 
 totalProfitLose = totalProfitLose + finances[i][1]
+totalChangeValues += (finances[i][1] - previousMonth)
 
+if( decrease > (finances[i][1] - previousMonth) ){
+     leastMonth = {
+        month : finances[i][0],
+        amount: finances[i][1],
+        change: finances[i][1] - previousMonth
+     }
+     decrease = finances[i][1] - previousMonth
+}
 
-
-
+if ( increase < (finances[i][1] - previousMonth)) {
+    greatestMonth = {
+        month : finances [i][0],
+        amount : finances [i][1],
+        change : finances[i][1] - previousMonth
+    }
+    increase = finances[i][1] - previousMonth
+}
+previousMonth = finances[i][1]
 };
+
+console.log(`Greatest increase in profit is during ${greatestMonth.month} with $${greatestMonth.amount} with $${greatestMonth.change}. `);
+console.log(`Greatest decrease in profit is during ${leastMonth.month} with $${leastMonth.amount} $${leastMonth.change}`);
 let avg = Math.round( (totalProfitLose/totalMonths) * 100) /100
 console.log(`Total Profit and Lostt: $${totalProfitLose}`);
 
-console.log(`Average: $${avg}`);
+console.log(`Average Profit/Loss : $${avg}`);
+
+console.log(`Average Change of values: $${Math.round((totalChangeValues / totalMonths)*100)/100}`);
